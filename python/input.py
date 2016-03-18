b835b33a887dfc1c592b08fb768aaf00d6c4c9c5
Stefano Martina <stefano.martina@gmail.com>
2016-03-18 18:56:41 +0100

    Modified python input test.

--- python/input.py
+++ python/input.py
@@ -2,7 +2,14 @@
 
 a = eval(input('a: '))
 b = float(input('b: '))
+c = int(input('c: '))
+d = bool(eval(input('d: ')))
+e = input('e: ')
 
-print('a: {}'.format(a))
-print('b: {}'.format(b))
+print('a: {} ({})'.format(a,type(a)))
+print('b: {} ({})'.format(b,type(b)))
+print('c: {} ({})'.format(c,type(c)))
+print('d: {} ({})'.format(d,type(d)))
+print('e: {} ({})'.format(e,type(e)))
+
 
