5ce86c0ed285b467216374af460d26edddede98c
Stefano Martina <stefano.martina@gmail.com>
2016-12-09 18:59:38 +0100

    updated test

--- python/refValueParams.py
+++ python/refValueParams.py
@@ -17,3 +17,19 @@ test1.changeValue()
 print(a)
 
 
+test2 = Test()
+b = {0:'un', 1:'du', 2:'tre'}
+test2.setValue(b)
+b[3] = 'quat'
+print(test2.att)
+
+test2.changeValue()
+print(b)
+
+test3 = Test()
+c = ([1,2,3], [4,5,6])
+test3.setValue(c)
+c[0][0] = 4
+#c[1] = 0
+print(test3.att)
+
